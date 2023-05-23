9d6fcba6f88343d327634e00645d73c39ed5de9b
Archi Patel <archi26983@gmail.com>
2023-05-23 08:38:38 +0530

    update in file1.java

--- src/a/File1.java
+++ src/a/File1.java
@@ -3,9 +3,8 @@ package a;
 public class File1 {
 
 	public static void main(String[] args) {
-		System.out.println("In Branch A");
-		System.out.println("In Branch file 1 changed");
-		System.out.println("In Branch A main method");
 		System.out.println("In Branch A conflict testing");
+		System.out.println("In Branch A");
+		System.out.println("conflict resolved");
 	}
 }
