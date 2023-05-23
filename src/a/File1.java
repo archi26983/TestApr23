0d5bc5ca22e25a6dd8b7f12f48251ec0847f1fee
Archi Patel <archi26983@gmail.com>
2023-05-23 08:12:19 +0530

    Conflict test

--- src/a/File1.java
+++ src/a/File1.java
@@ -4,5 +4,8 @@ public class File1 {
 
 	public static void main(String[] args) {
 		System.out.println("In Branch A");
+		System.out.println("In Branch file 1 changed");
+		System.out.println("In Branch A main method");
+		System.out.println("In Branch A conflict testing");
 	}
 }
--- src/basicConcepts/StringPac.java
+++ src/basicConcepts/StringPac.java
@@ -5,6 +5,8 @@ public class StringPac {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I am in main method");
+System.out.println("I am in stringpac1");
+System.out.println("I am in main method stringpac2");
 	}
 
 }
